e5e2160482eb0c93ce02d1cfaf36aba458718f54
your git username <your git email>
2021-02-12 13:27:04 +0900

    Room Display Screen

--- frontend/src/store/modules/info.js
+++ frontend/src/store/modules/info.js
@@ -4,42 +4,42 @@ import io from 'socket.io-client';
 const socket = io('http://localhost:3000')
 
 export default {
-	state() {
-		return {
-			rooms: []
-		}
-	},
-	mutations: {
-		setRooms(state, rooms) {
-			const newRooms = rooms.concat();
-			state.rooms = newRooms
-		}
-	},
-	getters: {
-		rooms(state) {
-			return state.rooms
-		}
-	},
-	actions: {
-		fetchRoom({ commit }) {
-			axios.post('/fetchRoom')
-				.then((response) => {
-					commit('setRooms', response.data.rooms);
-				})
-				.catch(() => {
-					return Promise.reject()
-				});
-		},
-		makeRoom({ commit }, { roomName, roomPassword }) {
-			const data = { name: roomName, password: roomPassword }
-			axios.post('/makeRoom', data)
-				.then((response) => {
-					socket.emit('UPDATE_ROOM');
-					commit('setRooms', response.data.rooms);
-				})
-				.catch(() => {
-					return Promise.reject()
-				});
-		}
-	}
+  state() {
+    return {
+      rooms: []
+    }
+  },
+  mutations: {
+    setRooms(state, rooms) {
+      const newRooms = rooms.concat();
+      state.rooms = newRooms
+    }
+  },
+  getters: {
+    rooms(state) {
+      return state.rooms
+    }
+  },
+  actions: {
+    fetchRoom({ commit }) {
+      axios.post('/fetchRoom')
+        .then((response) => {
+          commit('setRooms', response.data.rooms);
+        })
+        .catch(() => {
+          return Promise.reject()
+        });
+    },
+    makeRoom({ commit }, { roomName, roomPassword }) {
+      const data = { name: roomName, password: roomPassword }
+      axios.post('/makeRoom', data)
+        .then((response) => {
+          socket.emit('UPDATE_ROOM');
+          commit('setRooms', response.data.rooms);
+        })
+        .catch(() => {
+          return Promise.reject()
+        });
+    }
+  }
 }
